97a76b8f41a7349f0acaf2b5c3717c92828f1383
Rachel Ehrlich <rae84@cornell.edu>
2016-09-10 19:14:47 -0400

    display gender

--- get_actors.py
+++ get_actors.py
@@ -1,5 +1,6 @@
 import sys
 from imdb import IMDb
+import time
 
 def get_actors(movie):
     pass
@@ -11,11 +12,14 @@ def get_movie(name):
     ia.update(movie)
     print movie['long imdb canonical title']
     cast = movie['cast']
+    male_female = [0, 0]
+    print len(cast)
     for character in cast:
-        actor = ia.get_person(character.personID)
+        person = ia.get_person(character.personID)
+        male_female[person.has_key('actress')] += 1
         time.sleep(1)
 
-        print character.personID
+    print "men:", male_female[0], "women:", male_female[1]
 
 #get_movie(sys.argv[1])
 #get_movie("Captain America")
